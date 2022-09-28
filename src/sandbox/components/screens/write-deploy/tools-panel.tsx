e55876494dd27148b39aa48196f26ff54e2c0aa9
He1DAr <heidarsaleh@outlook.com>
2022-09-28 11:39:25 -0400

    feat: save project button

--- src/sandbox/components/screens/write-deploy/tools-panel.tsx
+++ src/sandbox/components/screens/write-deploy/tools-panel.tsx
@@ -26,6 +26,20 @@ export const WriteAndDeployTools: React.FC = props => {
       contractName,
     });
   }, [codeBody, contractName, codeBody]);
+
+  const goToPlatform = () => {
+    window
+      ?.open(
+        `${
+          process.env.NEXT_PUBLIC_PLATFORM_BASE_URL
+        }/editor/draft?contractName=${encodeURIComponent(
+          contractName
+        )}&codeBody=${encodeURIComponent(codeBody)}`,
+        '_blank'
+      )
+      ?.focus();
+  };
+
   return (
     <Flex flexDirection="column" flexGrow={1} p="loose" {...props}>
       <Title mb="extra-loose" fontSize="24px">
@@ -35,6 +49,9 @@ export const WriteAndDeployTools: React.FC = props => {
       <ContractName />
       <Button onClick={isSignedIn ? onDeploy : doOpenAuth} mt="base-loose" width="100%">
         {isSignedIn ? 'Deploy' : 'Connect Stacks Wallet'}
+      </Button>
+      <Button onClick={goToPlatform} mt="base-loose" width="100%">
+        Save Project
       </Button>
     </Flex>
   );
